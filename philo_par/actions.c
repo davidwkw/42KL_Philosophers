edcc4bdaae0bff80fcbf28f3058a0842926053d6
David Wang <devtesting404@gmail.com>
2021-12-04 14:10:42 +0800

    reduce redundant full increment in eat function

--- philo_par/actions.c
+++ philo_par/actions.c
@@ -25,9 +25,7 @@ void	eat_p(t_args *args, pthread_mutex_t *p_mutex, t_philo *philo)
 	if (!(print_status("is eating", p_mutex, philo->num, args)))
 	{
 		philo->last_eat_time = get_time();
-		if (args->conds.max_eat_num >= 0)
-			philo->eat_num++;
-		if (args->conds.max_eat_num >= 0 && philo->eat_num >= args->conds.max_eat_num)
+		if (args->conds.max_eat_num >= 0 && ++philo->eat_num >= args->conds.max_eat_num)
 		{
 			philo->full = 1;
 			args->all_full++;
